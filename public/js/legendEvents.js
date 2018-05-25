d1e6c2741a68371525c949626bfadfd0b1b69e9a
Shadrack Lilan <slilan96@gmail.com>
2018-05-25 12:35:02 -0400

    added comments

--- public/js/legendEvents.js
+++ public/js/legendEvents.js
@@ -1,13 +1,15 @@
 /**
- * to be reviewed
+ * This file contains code that handles the click events from the legend row. 
+ * This click handler will pick events from the div and then highlight the nodes 
+ * and edges that correspond to the selected pathway
  */
+
 $('#legend-row').click(function (e) {
-  //
+  
+  //variables to get data from the text or the div once clicked
   var $actualDiv = $(e.target).attr('data-pathway');
   var $childElement = $(e.target).parent().attr('data-pathway');
-  console.log($actualDiv);
 
-  //check if the div or the <p> within the div has been clicked
   if ($actualDiv || $childElement ) {
     var pathway = $actualDiv || $childElement;
     var wantedNodes = pathwaysObj[pathway];
@@ -20,15 +22,17 @@ $('#legend-row').click(function (e) {
     $('#pathway1').html('<h4>' + wantedNodes[0].name + '</h4>');
 
     for (var i = 1; i < wantedNodes.length; i++) {
-      var currentNode = wantedNodes[i]
+      var currentNode = wantedNodes[i];
       var type1 = currentNode.label.match(re1);
       var type2 = currentNode.label.match(re2);
       
       var $li = $('<li></li>').text(currentNode.label);
       $li.data('description', currentNode.courseDescription);
-      //attach click handler.
+      
+      //show description of courses when clicked.
       $li.click(showDescription);
       $('#pathway1').append($li);
+      
       //check structure of node/course ID. xxx or xxx-xxx
       if (type1) {
         nodeIDs.push(type1[0]);
@@ -37,6 +41,5 @@ $('#legend-row').click(function (e) {
       }
     }
   }
-
   network.selectNodes(nodeIDs);
 });
