21c7af2837f3b2081f8d76b6cea64ad1ead82cf7
Shadrack Lilan <slilan96@gmail.com>
2018-06-06 14:17:59 -0400

    created backup folder and included all the courses

---
+++ backup/data/pathway_list.js
@@ -0,0 +1,107 @@
+/**
+ * This file stores the names of the courses corresponding to the pathways.
+ * It's meant to facilitate quick lookup of courses and their descriptions when generating lists.
+ * It is an object that stores a mapping of pathways to their courses.
+ *
+ * The first item in each Pathway's array is a simple object that stores the full name of the pathway
+ */
+
+//Remaining pathways to be added later.
+var pathwaysObj = {
+  law_society: [
+    {name: 'Law and Society'},
+    { color: '#8b4789'}
+  ],
+  gender_sexuality: [
+    { name: 'Gender and Sexuality'},
+    { color: '#ffff66'},
+    { label: 'HIST-011: Deciphering America',
+      courseDescription: 'This course examines American history from the first contacts of the indigenous peoples of North America with European settlers to our own times by focusing on a few telling moments in this history. The course treats twelve of these moments. Each unit begins with a specific primary document, historical figure, image, location, year, or cultural artifact to commence the delving into the American past. Some of these icons are familiar, but the ensuing deciphering will render them as more complicated; some are unfamiliar, but they will emerge as absolutely telling. The course meets each week for two 50-minute team-taught lectures and once recitation session. Course requirements include: in-class midterm and final exams; three short paper assignments; and punctual attendance and participation in recitations.'},
+    { label: 'HIST-145: Discovering the Middle Ages',
+      courseDescription: 'Discovering the Middle Ages Description'},
+  ],
+  borders_migration: [
+    {name: 'Borders, Migration and Immigration'},
+    { color: '#00abff'}
+  ],
+  intell_culture: [
+    { name: 'Intellectual and Cultural Life'},
+    { color: '#ffa500'},
+    { label: 'HIST-031: Making or Breaking European Hegemony',
+      courseDescription: 'Hist 031 will trace the dramatic rise and fall of Europe\'s global hegemony during the period roughly from 1450 to 1950. Among the major themes we will examine are: states and power, borders and resistance, race and genocide, economies and oppression, ideas and revolution, the building and change of hierarchies of gender and power. Truly, a dramatic story. The objectives of the course are: 1) To serve as an introduction to the study of history for majors and non-majors alike, and to teach the critical analysis of historical sources; 2) to teach substantive knowledge of European history; 3) to provide a foundation for further study of the European past. No previous background in European or World history is required.'},
+    { label: 'HIST-040: Early Modern Europe, 1450 - 1750',
+      courseDescription: 'This course examines those European developments which contributed to the world we understand as modern. Special emphasis will be placed on the transformation of Europe through the advent of new technologies, the creation of a global economy, the consolidation of territorial states, the rise of effective, central governments, the dissolution of religious unity, and the dialect between modern and traditional world views.'},
+    { label: 'HIST-048: Rise and Fall of the Russian Empire, 1522 - 1917',
+      courseDescription: 'Rise and Fall of the Russian Empire, 1522 - 1917'},
+    { label: 'HIST-049: The Soviet Century 1917 - 1991',
+      courseDescription: 'Out of an obscure, backward empire, the Soviet Union emerged to become the great political laboratory of the twentieth century. This course will trace the roots of the world\'s first socialist society and its attempts to recast human relations and human nature itself. Topics include the origins of the Revolution of 1917, the role of ideology in state policy and everyday life, the Soviet Union as the center of world communism, the challenge of ethnic diversity, and the reasons for the USSR\'s sudden implosion at the end of the century.Focusing on politics, society, culture, and their interaction, we will examine the rulers (from Lenin to Gorbachev) as well as the ruled (peasants, workers, and intellectuals; Russians and non-Russians). The course will feature discussions of selected texts, including primary sources in translation.'
+    }
+  ],
+  econ_history: [
+    { name: 'Economic History'},
+    { color: '#00FF33'},
+    { label: 'HIST-030: Emergence of Modern Europe',
+      courseDescription: 'Emergence of Modern Europe stuff'},
+    { label: 'HIST-040: Early Modern Europe, 1450 - 1750',
+      courseDescription: 'This course examines those European developments which contributed to the world we understand as modern. Special emphasis will be placed on the transformation of Europe through the advent of new technologies, the creation of a global economy, the consolidation of territorial states, the rise of effective, central governments, the dissolution of religious unity, and the dialect between modern and traditional world views.'},
+    { label: 'HIST-049: The Soviet Century 1917 - 1991',
+      courseDescription: 'Out of an obscure, backward empire, the Soviet Union emerged to become the great political laboratory of the twentieth century. This course will trace the roots of the world\'s first socialist society and its attempts to recast human relations and human nature itself. Topics include the origins of the Revolution of 1917, the role of ideology in state policy and everyday life, the Soviet Union as the center of world communism, the challenge of ethnic diversity, and the reasons for the USSR\'s sudden implosion at the end of the century.Focusing on politics, society, culture, and their interaction, we will examine the rulers (from Lenin to Gorbachev) as well as the ruled (peasants, workers, and intellectuals; Russians and non-Russians). The course will feature discussions of selected texts, including primary sources in translation.'
+    }
+  ],
+  war_peace: [
+    { name: 'War, Peace and Diplomacy'},
+    { color: '#ffc0cb'},
+    { label: 'HIST-030: Emergence of Modern Europe',
+      courseDescription: 'Emergence of Modern Europe stuff'},
+    { label: 'HIST-031: Making or Breaking European Hegemony',
+      courseDescription: 'Hist 031 will trace the dramatic rise and fall of Europe\'s global hegemony during the period roughly from 1450 to 1950. Among the major themes we will examine are: states and power, borders and resistance, race and genocide, economies and oppression, ideas and revolution, the building and change of hierarchies of gender and power. Truly, a dramatic story. The objectives of the course are: 1) To serve as an introduction to the study of history for majors and non-majors alike, and to teach the critical analysis of historical sources; 2) to teach substantive knowledge of European history; 3) to provide a foundation for further study of the European past. No previous background in European or World history is required.'},
+    { label: 'HIST-048: Rise and Fall of the Russian Empire, 1522 - 1917',
+      courseDescription: 'Rise and Fall of the Russian Empire, 1522 - 1917 Description'},
+    { label: 'HIST-051: Modern Britain',
+      courseDescription: 'This course surveys British history from the eighteenth century to the present. In the first two centuries we\'ll study, Britain built an empire that ruled over a quarter of the globe and became the world\'s leading economy. In 2016, the Empire is gone, but coincident with its loss, Britain gained a welfare state and retained a role as a powerhouse of culture and finance. Themes emphasized in the course include national identity, class, political reform, war, and imperialism. How did Britain come to dominate the global economy and what did this dominance look like? How should we understand its role in the world today? Do the Beatles, the Royal Family, and Amy Winehouse add up to a "Cool Britannia" that has somehow "made up" for the loss of the Empire? How will the recent Brexit vote shape Britain\'s status in the future? Using written and visual primary sources, we\'ll look at all of these questions as we study the birth and development of modern Britain.'
+    }
+  ],
+  religious_comm: [
+    { name: 'Religious Communities'},
+    { color: '#40e0d0'},
+    { label: 'HIST-040: Early Modern Europe, 14\'50 - 1750',
+      courseDescription: 'Early Modern Europe, 1450 - 1750'},
+    { label: 'HIST-050: British Isles',
+      courseDescription: 'British Isles Description Goes Here'}
+  ],
+  human_rights: [
+    { name: 'Human Rights/Humanitarianism'},
+    { color: '#a52a2a'}
+  ],
+  historical_methods: [
+    { name: 'Historical Methods'},
+    { color: '#008000'},
+  ],
+  slavery_race: [
+    { name: 'Slavery and Race'},
+    { color: '#c4a464'},
+    { label: 'HIST-011: Deciphering America',
+      courseDescription: 'This course examines American history from the first contacts of the indigenous peoples of North America with European settlers to our own times by focusing on a few telling moments in this history. The course treats twelve of these moments. Each unit begins with a specific primary document, historical figure, image, location, year, or cultural artifact to commence the delving into the American past. Some of these icons are familiar, but the ensuing deciphering will render them as more complicated; some are unfamiliar, but they will emerge as absolutely telling. The course meets each week for two 50-minute team-taught lectures and once recitation session. Course requirements include: in-class midterm and final exams; three short paper assignments; and punctual attendance and participation in recitations.'},
+    { label: 'HIST-070: HIST-070: Colonial Latin America',
+      courseDescription: 'Colonial Latin America Description Goes Here'}
+  ],
+  space_place: [
+    { name: 'Space and Place(urban, environmental history'},
+    {color: '#c792c7' }
+  ],
+  politics_rev: [
+    { name: 'Politics and Revolution'},
+    { color: '#FF6666'},
+    { label: 'HIST-011: Deciphering America',
+      courseDescription: 'This course examines American history from the first contacts of the indigenous peoples of North America with European settlers to our own times by focusing on a few telling moments in this history. The course treats twelve of these moments. Each unit begins with a specific primary document, historical figure, image, location, year, or cultural artifact to commence the delving into the American past. Some of these icons are familiar, but the ensuing deciphering will render them as more complicated; some are unfamiliar, but they will emerge as absolutely telling. The course meets each week for two 50-minute team-taught lectures and once recitation session. Course requirements include: in-class midterm and final exams; three short paper assignments; and punctual attendance and participation in recitations.'},
+    { label: 'HIST-030: Emergence of Modern Europe',
+      courseDescription: 'Emergence of Modern Europe stuff'},
+    { label: 'HIST-031: Making or Breaking European Hegemony',
+      courseDescription: 'Hist 031 will trace the dramatic rise and fall of Europe\'s global hegemony during the period roughly from 1450 to 1950. Among the major themes we will examine are: states and power, borders and resistance, race and genocide, economies and oppression, ideas and revolution, the building and change of hierarchies of gender and power. Truly, a dramatic story. The objectives of the course are: 1) To serve as an introduction to the study of history for majors and non-majors alike, and to teach the critical analysis of historical sources; 2) to teach substantive knowledge of European history; 3) to provide a foundation for further study of the European past. No previous background in European or World history is required.'},
+    { label: 'HIST-048: Rise and Fall of the Russian Empire, 1522 - 1917',
+      courseDescription: 'Rise and Fall of the Russian Empire, 1522 - 1917'},
+    { label: 'HIST-049: The Soviet Century 1917 - 1991',
+      courseDescription: 'Out of an obscure, backward empire, the Soviet Union emerged to become the great political laboratory of the twentieth century. This course will trace the roots of the world\'s first socialist society and its attempts to recast human relations and human nature itself. Topics include the origins of the Revolution of 1917, the role of ideology in state policy and everyday life, the Soviet Union as the center of world communism, the challenge of ethnic diversity, and the reasons for the USSR\'s sudden implosion at the end of the century.Focusing on politics, society, culture, and their interaction, we will examine the rulers (from Lenin to Gorbachev) as well as the ruled (peasants, workers, and intellectuals; Russians and non-Russians). The course will feature discussions of selected texts, including primary sources in translation.'
+    }
+  ]
+};
